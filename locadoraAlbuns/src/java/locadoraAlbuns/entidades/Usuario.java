5546da477c5bbdd1a6f48e5be14fe72ea4747e67
Nathália Pissuti <nathaliapissuti@gmail.com>
2017-12-01 21:43:28 -0200

    Classe de Usuario

---
+++ locadoraAlbuns/src/java/locadoraAlbuns/entidades/Usuario.java
@@ -0,0 +1,72 @@
+package locadoraAlbuns.entidades;
+
+/**
+ *
+ * @author nathipg
+ */
+public class Usuario {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String endereco;
+    private String telefoneCelular;
+    private String telefoneFixo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefoneCelular() {
+        return telefoneCelular;
+    }
+
+    public void setTelefoneCelular(String telefoneCelular) {
+        this.telefoneCelular = telefoneCelular;
+    }
+
+    public String getTelefoneFixo() {
+        return telefoneFixo;
+    }
+
+    public void setTelefoneFixo(String telefoneFixo) {
+        this.telefoneFixo = telefoneFixo;
+    }
+    
+}
