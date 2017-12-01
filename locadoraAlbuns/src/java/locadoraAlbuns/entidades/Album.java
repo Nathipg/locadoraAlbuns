16246f024121bb70dbc1c7896a7072c35c6789b6
Nathália Pissuti <nathaliapissuti@gmail.com>
2017-12-01 21:51:20 -0200

    Classe de Album

--- locadoraAlbuns/src/java/locadoraAlbuns/entidades/Album.java
+++ locadoraAlbuns/src/java/locadoraAlbuns/entidades/Album.java
@@ -5,5 +5,69 @@ package locadoraAlbuns.entidades;
  * @author nathipg
  */
 public class Album {
+    private int id;
+    private String nome;
+    private String foto;
+    private String dataLancamento;
+    private Genero genero;
+    private Banda banda;
+    private Musico musico;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(String dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public Banda getBanda() {
+        return banda;
+    }
+
+    public void setBanda(Banda banda) {
+        this.banda = banda;
+    }
+
+    public Musico getMusico() {
+        return musico;
+    }
+
+    public void setMusico(Musico musico) {
+        this.musico = musico;
+    }
+    
     
 }
