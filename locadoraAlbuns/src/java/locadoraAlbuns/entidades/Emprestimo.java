1f0c84567ebc495ae071bf71b0d4e84565a01014
Nathália Pissuti <nathaliapissuti@gmail.com>
2017-12-01 21:48:00 -0200

    Classe de Emprestimo

---
+++ locadoraAlbuns/src/java/locadoraAlbuns/entidades/Emprestimo.java
@@ -0,0 +1,55 @@
+package locadoraAlbuns.entidades;
+
+/**
+ *
+ * @author nathipg
+ */
+public class Emprestimo {
+    private int id;
+    private String inicio;
+    private String fim;
+    private Usuario usuario;
+    private Album album;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFim() {
+        return fim;
+    }
+
+    public void setFim(String fim) {
+        this.fim = fim;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+    
+    
+}
