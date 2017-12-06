2406613819a9d53379f812260ade59a670f0f79f
Nathália Pissuti <nathaliapissuti@gmail.com>
2017-12-06 19:43:37 -0200

    DAO de Genero

--- locadoraAlbuns/src/java/locadoraAlbuns/dao/GeneroDAO.java
+++ locadoraAlbuns/src/java/locadoraAlbuns/dao/GeneroDAO.java
@@ -1,7 +1,9 @@
 package locadoraAlbuns.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import locadoraAlbuns.entidades.Genero;
 
@@ -29,22 +31,89 @@ public class GeneroDAO extends DAO<Genero> {
 
     @Override
     public void atualizar(Genero obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement stmt = getConnection().prepareStatement(
+                "UPDATE genero "
+                + "SET"
+                + "    nome = ?,"
+                + "WHERE"
+                + "    id = ?;" );
+
+        stmt.setString( 1, obj.getNome() );
+        stmt.setInt( 3, obj.getId() );
+
+        stmt.executeUpdate();
+        stmt.close();
     }
 
     @Override
     public void excluir(Genero obj) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement stmt = getConnection().prepareStatement(
+                "DELETE FROM genero "
+                + "WHERE"
+                + "    id = ?;" );
+
+        stmt.setInt( 1, obj.getId() );
+
+        stmt.executeUpdate();
+        stmt.close();
     }
 
     @Override
     public List<Genero> listarTodos() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Genero> lista = new ArrayList<Genero>();
+
+        PreparedStatement stmt = getConnection().prepareStatement(
+                "SELECT "
+                + "    genero.id,"
+                + "    genero.nome"
+                + "FROM genero" );
+
+        ResultSet rs = stmt.executeQuery();
+
+        while ( rs.next() ) {
+            
+            Genero genero = new Genero();
+
+            genero.setId( rs.getInt( "id" ) );
+            genero.setNome( rs.getString( "nome" ) );
+
+            lista.add( genero );
+
+        }
+
+        rs.close();
+        stmt.close();
+
+        return lista;
     }
 
     @Override
     public Genero obterPorId(int id) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Genero genero = null;
+
+        PreparedStatement stmt = getConnection().prepareStatement(
+                "SELECT "
+                + "    genero.id, "
+                + "    genero.nome "
+                + "FROM genero "
+                + "WHERE genero.id = ?;");
+
+        stmt.setInt( 1, id );
+
+        ResultSet rs = stmt.executeQuery();
+
+        if ( rs.next() ) {
+            genero = new Genero();
+
+            genero.setId( rs.getInt( "id" ) );
+            genero.setNome( rs.getString( "nome" ) );
+        }
+
+        rs.close();
+        stmt.close();
+
+        return genero;
     }
     
 }
